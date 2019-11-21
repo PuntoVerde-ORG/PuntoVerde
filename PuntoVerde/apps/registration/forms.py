b94d42b529691bddf5b4f70796cf73c80c2c3a3a
FelipePc <felipeandrescatalan.18@gmail.com>
2019-11-21 18:45:46 -0300

    App Registro Completa + Creacion y edicion de perfiles y avatar

--- PuntoVerde/apps/registration/forms.py
+++ PuntoVerde/apps/registration/forms.py
@@ -1,9 +1,10 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-
+from .models import Profile
 class UserCreationFormWithEmail(UserCreationForm):
 	email = forms.EmailField(required=True, help_text="Ingrese un correo Valido")
+
 
 	class Meta:
 		model = User
@@ -14,3 +15,16 @@ class UserCreationFormWithEmail(UserCreationForm):
 		if User.objects.filter(email=email).exists():
 			raise forms.ValidationError("El email ya esta en uso, intente nuevamente.")
 		return email
+
+
+class ProfileForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = fields = ['avatar', 'descripcion', 'link' ]
+		widgets = { 
+		   'avatar': forms.ClearableFileInput(attrs={'class':'form-control-file mt-3'}),
+		   'descripcion': forms.Textarea(attrs={'class':'form-control mt-3', 'rows':3, 'placeholder':'Cuéntanos sobre ti'}),
+		   'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Comparte tu Red Social'}),
+
+
+		}
